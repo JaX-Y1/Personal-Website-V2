2091c016f012f5acedfe64defdba111e84d83b03
JaX-Y1 <136761731+JaX-Y1@users.noreply.github.com>
2025-01-06 00:59:50 -0600

    Reworked Home Page Animation

--- docs/assets/js/home_animation.js
+++ docs/assets/js/home_animation.js
@@ -1,9 +1,11 @@
 //driver code
 const myCanvas = document.getElementById("lander-canvas");
 const ctx = myCanvas.getContext("2d");
-var numNodes = 1000;
+var numNodes = 50;
+var lineDistance = 150; //max distance to connect nodes
 var nodeArray = []; //array to hold the node objects
 const refRadius = 1;
+var radiusTolerance = refRadius+1;
 const nodeCordPadding = 5;
 const velocityRange = [5,10]; //in pixel per second
 const velocityLimiter = 10; //less means faster movement
@@ -52,7 +54,6 @@ function updatePosition(ctx){
 }
 
 function keepInBounds(node){
-    var radiusTolerance = refRadius+1;
     if(node.xCord + ((node.xVel/velocityLimiter))<= radiusTolerance){
         node.xVel = -node.xVel;
     }else if(node.xCord + ((node.xVel/velocityLimiter))>= myCanvas.width-radiusTolerance){
@@ -66,28 +67,47 @@ function keepInBounds(node){
 }
 
 function drawCanvas(ctx){
+    let alphaValue = 1.0;
     ctx.clearRect(0,0, myCanvas.width, myCanvas.height);
+    ctx.globalCompositeOperation = "lighter";
     for(let i = 0; i < numNodes; ++i){
         ctx.beginPath();
         ctx.arc(nodeArray[i].xCord, nodeArray[i].yCord, refRadius, 0, 2 * Math.PI);
+        //ctx.lineWidth = 1;
         ctx.fillStyle = "yellow";
         ctx.strokeStyle = "yellow";
+        ctx.lineWidth = 1;
         ctx.fill();
         ctx.stroke();
+        //draw connecting lines for current node
+        for(let j = 0; j < numNodes; ++j){
+            if(nodeArray[i]!=nodeArray[j]){
+                if(distanceBtwnNodes(nodeArray[i],nodeArray[j])<lineDistance){
+                    ctx.beginPath();
+                    ctx.moveTo(nodeArray[i].xCord, nodeArray[i].yCord);
+                    ctx.lineTo(nodeArray[j].xCord, nodeArray[j].yCord);
+                    alphaValue = scale(distanceBtwnNodes(nodeArray[i],nodeArray[j]),0,lineDistance,1.0,0.0);
+                    ctx.strokeStyle = `rgba(255,255,0, ${alphaValue})`;
+                    ctx.lineWidth = 1;
+                    ctx.stroke();
+                }
+            }
+        }
     }
 }
 
-//helper functions
-function randomRangeInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function refresh(referenceWidth, referenceHeight, drawFunction, timestamp) {
-    myCanvas.width = myCanvas.clientWidth;
-    myCanvas.height = myCanvas.clientHeight;
-  
+    if(myCanvas.clientWidth != myCanvas.width || myCanvas.clientHeight != myCanvas.height){
+        myCanvas.width = myCanvas.clientWidth;
+        myCanvas.height = myCanvas.clientHeight;
+        for(let i = 0; i < numNodes; ++i){
+            if((nodeArray[i].xCord <= myCanvas.width + radiusTolerance) && (nodeArray[i].xCord >= myCanvas.width - radiusTolerance)){
+                nodeArray[i].xCord += 2 * radiusTolerance;
+                //prevent nodes getting stuck in screen width while resizing screen
+                //also allows for fun interactions with nodes when stretching screen
+            }
+        }
+    }
     const ratio = Math.min(
       myCanvas.width / referenceWidth,
       myCanvas.height / referenceHeight
@@ -105,4 +125,24 @@ function refresh(referenceWidth, referenceHeight, drawFunction, timestamp) {
         refresh(referenceWidth, referenceHeight, drawFunction, time);
       });
   }
-  
+
+//helper functions
+function randomRangeInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function distanceBtwnNodes(node1, node2){
+    var xs = 0;
+    var ys = 0;
+    xs = node2.xCord - node1.xCord;
+    xs = xs * xs;
+    ys = node2.yCord - node1.yCord;
+    ys = ys * ys;
+    return Math.sqrt( xs + ys );
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
